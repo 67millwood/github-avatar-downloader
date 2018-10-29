120053a3f56b6912b9ad0933a21e2197ac06ca3a
67millwood <info@track2development.com>
2018-10-29 21:33:45 +0000

    whatever

--- download_avatars.js
+++ download_avatars.js
@@ -6,13 +6,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': '67millwood'
+      'User-Agent': '67millwood',
+      'Authorization': "token " + mysecrets.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
-  });
+    var jsbody = JSON.parse(body);
+    var emptyArr = [];
+    for (i = 0; i < jsbody.length; i++) {
+      emptyArr.push(jsbody[i].avatar_url);
+    }
+    cb(err, emptyArr)
+    });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
