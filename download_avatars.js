cb1bdff7c4bc835cc77cf4f620db77193433a2b6
67millwood <info@track2development.com>
2018-10-29 21:52:19 +0000

    static image working

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var mysecrets = require('./secrets.js')
+var fs = require('fs');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -19,12 +20,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
     cb(err, emptyArr)
     });
-}
+  }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+function downloadImageByUrl() {
 
+    request.get('https://avatars2.githubusercontent.com/u/2741?v=3&s=466/avatars/kvirani.jpg')
+       .on('error', function (err) {
+         console.log("this is not working");
+         throw err;
+       })
+       .pipe(fs.createWriteStream('./thispic.jpg'));
 
+  }
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
+
+downloadImageByUrl();
 // getRepoContributors();
