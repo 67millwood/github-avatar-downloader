86f289c0f08e13a157a37a9677dd0eb532dbbc76
67millwood <info@track2development.com>
2018-10-29 20:21:48 +0000

    fun

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,24 @@
 var request = require('request');
+var mysecrets = require('./secrets.js')
 
-console.log("I will write any message i like");
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': '67millwood'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
+// getRepoContributors();
